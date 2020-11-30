e3cc680ac6c7e4304a74acfa3eccfacd605e3c03
rodriguezespinoza0795 <rodriguezespinoza0795@gmail.com>
2020-11-29 20:14:17 -0600

    using esLint

--- index.js
+++ index.js
@@ -1,36 +1,35 @@
-require('dotenv').config()
-const { makeExecutableSchema } = require('graphql-tools')
-const express = require('express') // importar express
-const cors = require('cors')
-const { graphqlHTTP }  = require('express-graphql') // importar middleware express
+require("dotenv").config();
+const { makeExecutableSchema } = require("graphql-tools");
+const express = require("express"); // importar express
+const cors = require("cors");
+const { graphqlHTTP } = require("express-graphql"); // importar middleware express
 
+const app = express(); // crear API express
+const PORT = process.env.DB_PORT || 3000; // puesto para trabajar
+const isDev = process.env.NODE_ENV !== "production";
 
-const app = express() // crear API express
-const PORT = process.env.DB_PORT || 3000 // puesto para trabajar
-const isDev = process.env.NODE_ENV !== 'production';
-
-const { readFileSync } = require('fs')
-const { join } = require('path')
+const { readFileSync } = require("fs");
+const { join } = require("path");
 
 // Definiendo schema
 const typeDefs = readFileSync(
-    join(__dirname, 'lib', 'schema.graphql'),
-    'utf-8'
-  )
+  join(__dirname, "lib", "schema.graphql"),
+  "utf-8",
+);
 
-  // Configurar resolvers
-const resolvers = require('./lib/resolvers')
+// Configurar resolvers
+const resolvers = require("./lib/resolvers");
 
-const schema = makeExecutableSchema ({typeDefs,resolvers})
+const schema = makeExecutableSchema({ typeDefs, resolvers });
 
-app.use(cors())
+app.use(cors());
 
-app.use('/api', graphqlHTTP({
-    schema: schema,
-    rootValue: resolvers,
-    graphiql: isDev
-  }))
-  
-  app.listen(PORT, () => {
-    console.log(`Server listen on port http://localhost:${PORT}/api`)
-  })
+app.use("/api", graphqlHTTP({
+  schema,
+  rootValue: resolvers,
+  graphiql: isDev,
+}));
+
+app.listen(PORT, () => {
+  console.log(`Server listen on port http://localhost:${PORT}/api`);
+});
